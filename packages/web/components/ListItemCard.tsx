28572c73355355f18f49c850a9d26ee6476c0bfe
M <75613073+DoodleDeBug@users.noreply.github.com>
2022-06-19 19:16:08 +0100

    begin listitemcard component

--- packages/web/components/ListItemCard.tsx
+++ packages/web/components/ListItemCard.tsx
@@ -16,7 +16,7 @@ export const ListItemCard = ({
       <Card variant="outlined" sx={{ minWidth: 275, my: 5 }}>
         <CardContent>
           <p>
-            {firstName} {lastName}{' '}
+            {firstName} {lastName}
           </p>
           <p>{items.length} items</p>
           <Button>Select</Button>
@@ -27,3 +27,37 @@ export const ListItemCard = ({
 };
 
 export default ListItemCard;
+
+{
+  /* <Button onClick={(e) => setOpen(true)}>
+                    <Card variant="outlined" sx={{ minWidth: 275, my: 5 }}>
+                      <CardContent>
+                        <p> {firstName} {lastName}</p>
+                        <p>{items.length} items</p>
+                      </CardContent>
+                    </Card>
+                  </Button>
+                  <Modal
+                    open={open}
+                    onClose={(e) => setOpen(false)}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                  >
+                    <Box sx={style}>
+                      <Typography
+                        id="modal-modal-title"
+                        variant="h6"
+                        component="h2"
+                      >
+                        List #{id}
+                      </Typography>
+                      <List>
+                        <ListItem>Bread</ListItem>
+                        <ListItem>Milk</ListItem>
+                        <ListItem>Sugar</ListItem>
+                        <ListItem>Eggs</ListItem>
+                        <ListItem>Nappies</ListItem>
+                      </List>
+                    </Box>
+                  </Modal> */
+}
